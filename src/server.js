1ef6c47d186cbeb59e9fcfe655604723bbe3e53d
yeonnex <yeonnex@gmail.com>
2021-09-26 16:27:09 +0900

    pug 로 view engine 설정, express에 템플릿 경로 지정, / 경로 설정

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express"
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "\\views")
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res)=> res.render("home.pug"))
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on port 3000...!`)
+app.listen(3000, handleListen);
+
+// Express 로 할 일은 여기까지가 끝이다.
+// Express 로 할 일은, views 를 설정해주고 render 해주는 거고 
+// 나머지는 websocket에서 실시간으로 일어날 것이다.
